a68d62677a21d040f52c4815ad54d3797d459076
Ragavee1998 <31559046+Ragavee1998@users.noreply.github.com>
2017-09-02 12:38:38 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -3,8 +3,8 @@ int main()
 {
 int year;
 if(year%4==0)
-printf(the year is a leap year");
+printf("the year is a leap year");
 else
-printf(the year is not a leap year");
+printf("the year is not a leap year");
 return 0;
 }
