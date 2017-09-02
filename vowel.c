ce72dfe16175a31e85cbb8ff1ff077a99bba4a17
Ragavee1998 <31559046+Ragavee1998@users.noreply.github.com>
2017-09-02 11:27:41 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char alpha;
+if((alpha=='a'||'A')||(alpha=='e'||'E')||(alpha=='i'||'I')||(alpha=='o'||'O')||(alpha=='u'||'U'))
+printf("the alphabet is vowel");
+else
+printf("the alphabet is consonant");
+return 0;
+}
