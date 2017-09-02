eb53be0274bc6bd783f4f9050f2d8e92621a7f10
Ragavee1998 <31559046+Ragavee1998@users.noreply.github.com>
2017-09-02 11:55:44 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char input;
+printf("enter the input:");
+scanf("%s",&input);
+if(((input>='a')&&(input<='z'))||((input>='A')&&(input<='Z'))
+printf("the input is alphabet");
+else
+printf("the input is alphabet");
+return 0;
+}
